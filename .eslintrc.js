b8eb9a98f3347cf6948371702b1195beae9de6f1
wotusay <wout.salembier@hotmail.com>
2021-11-05 13:47:56 +0100

    fix: added eslint config and ts config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  ignorePatterns: ["projects/**/*"],
+  ignorePatterns: ['projects/**/*'],
   plugins: [`prettier`, `import`],
   parserOptions: {
     ecmaVersion: 2020,
@@ -12,89 +12,89 @@ module.exports = {
     node: true,
     es6: true,
   },
-    rules: {
-      'arrow-body-style': `off`,
-      'object-shorthand': [`error`, `properties`],
-      'prefer-arrow-callback': [
-        `error`,
-        { allowNamedFunctions: true, allowUnboundThis: true },
-      ],
-      'prefer-destructuring': `off`, // I like it, but not that much...
-      'sort-imports': `off`,
-      'new-cap': `off`,
-      'no-invalid-this': `off`,
-      'no-class-assign': `error`,
-      'no-duplicate-imports': `error`,
-      'no-restricted-exports': `off`,
-      'no-restricted-imports': `off`,
-      'no-useless-computed-key': `error`,
-      'no-useless-constructor': `error`,
-      'no-useless-rename': `error`,
-      'no-var': `error`,
-      'prefer-const': `error`,
-      'prefer-numeric-literals': `error`,
-      'prefer-rest-params': `error`,
-      'prefer-spread': `error`,
-      'prefer-template': `error`,
-      'require-yield': `error`,
-      'symbol-description': `error`,
-      'constructor-super': `error`,
-      'no-const-assign': `error`,
-      'no-console': ['error', { allow: ['warn', 'info', 'error'] }],
-      'no-dupe-class-members': `error`,
-      'no-new-symbol': `error`,
-      'no-this-before-super': `error`,
-      'no-unsafe-optional-chaining': `error`,
-      'no-unused-expressions': `off`,
-      'no-unused-vars': [
-        `error`,
-        {
-          varsIgnorePattern: `^_`,
-          argsIgnorePattern: `^_`,
-          ignoreRestSiblings: true,
-        },
-      ],
-      'valid-typeof': `off`,
-      'require-jsdoc': `off`,
-      'valid-jsdoc': `off`,
-      'import/order': [
-        'error',
-        {
-          groups: [
-            'builtin',
-            'external',
-            'internal',
-            'sibling',
-            'parent',
-            'index',
-            'object',
-          ],
-          'newlines-between': 'always-and-inside-groups',
-          alphabetize: { order: 'asc', caseInsensitive: true },
-        },
-      ],
-      'prettier/prettier': [
-        'error',
-        {
-          arrowParens: 'avoid',
-          bracketSpacing: true,
-          embeddedLanguageFormatting: 'auto',
-          endOfLine: 'lf',
-          htmlWhitespaceSensitivity: 'css',
-          insertPragma: false,
-          jsxBracketSameLine: false,
-          jsxSingleQuote: false,
-          printWidth: 80,
-          proseWrap: 'always',
-          quoteProps: 'as-needed',
-          requirePragma: false,
-          semi: true,
-          singleQuote: true,
-          tabWidth: 2,
-          trailingComma: 'all',
-          useTabs: false,
-        },
-      ],
+  rules: {
+    'arrow-body-style': `off`,
+    'object-shorthand': [`error`, `properties`],
+    'prefer-arrow-callback': [
+      `error`,
+      { allowNamedFunctions: true, allowUnboundThis: true },
+    ],
+    'prefer-destructuring': `off`, // I like it, but not that much...
+    'sort-imports': `off`,
+    'new-cap': `off`,
+    'no-invalid-this': `off`,
+    'no-class-assign': `error`,
+    'no-duplicate-imports': `error`,
+    'no-restricted-exports': `off`,
+    'no-restricted-imports': `off`,
+    'no-useless-computed-key': `error`,
+    'no-useless-constructor': `error`,
+    'no-useless-rename': `error`,
+    'no-var': `error`,
+    'prefer-const': `error`,
+    'prefer-numeric-literals': `error`,
+    'prefer-rest-params': `error`,
+    'prefer-spread': `error`,
+    'prefer-template': `error`,
+    'require-yield': `error`,
+    'symbol-description': `error`,
+    'constructor-super': `error`,
+    'no-const-assign': `error`,
+    'no-console': ['error', { allow: ['warn', 'info', 'error'] }],
+    'no-dupe-class-members': `error`,
+    'no-new-symbol': `error`,
+    'no-this-before-super': `error`,
+    'no-unsafe-optional-chaining': `error`,
+    'no-unused-expressions': `off`,
+    'no-unused-vars': [
+      `error`,
+      {
+        varsIgnorePattern: `^_`,
+        argsIgnorePattern: `^_`,
+        ignoreRestSiblings: true,
+      },
+    ],
+    'valid-typeof': `off`,
+    'require-jsdoc': `off`,
+    'valid-jsdoc': `off`,
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'sibling',
+          'parent',
+          'index',
+          'object',
+        ],
+        'newlines-between': 'always-and-inside-groups',
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        arrowParens: 'avoid',
+        bracketSpacing: true,
+        embeddedLanguageFormatting: 'auto',
+        endOfLine: 'lf',
+        htmlWhitespaceSensitivity: 'css',
+        insertPragma: false,
+        jsxBracketSameLine: false,
+        jsxSingleQuote: false,
+        printWidth: 80,
+        proseWrap: 'always',
+        quoteProps: 'as-needed',
+        requirePragma: false,
+        semi: true,
+        singleQuote: true,
+        tabWidth: 2,
+        trailingComma: 'all',
+        useTabs: false,
+      },
+    ],
   },
   overrides: [
     {
@@ -105,12 +105,14 @@ module.exports = {
         sourceType: `module`,
       },
       plugins: [`@typescript-eslint/eslint-plugin`],
-      extends: [`plugin:@angular-eslint/recommended`,
-      `plugin:@angular-eslint/template/process-inline-templates`,
-      `eslint:recommended`,
-      `plugin:@typescript-eslint/recommended`,
-      `plugin:@typescript-eslint/recommended-requiring-type-checking`,
-      `plugin:prettier/recommended`],
+      extends: [
+        `plugin:@angular-eslint/recommended`,
+        `plugin:@angular-eslint/template/process-inline-templates`,
+        `eslint:recommended`,
+        `plugin:@typescript-eslint/recommended`,
+        `plugin:@typescript-eslint/recommended-requiring-type-checking`,
+        `plugin:prettier/recommended`,
+      ],
       rules: {
         '@typescript-eslint/no-invalid-this': `error`,
         'no-var': `error`,
@@ -141,16 +143,16 @@ module.exports = {
           {
             type: `element`,
             prefix: `app`,
-            style: `kebab-case`
-          }
+            style: `kebab-case`,
+          },
         ],
-        "@angular-eslint/directive-selector": [
+        '@angular-eslint/directive-selector': [
           `error`,
           {
             type: `attribute`,
             prefix: `app`,
-            style: `camelCase`
-          }
+            style: `camelCase`,
+          },
         ],
         '@typescript-eslint/explicit-module-boundary-types': [
           `warn`,
@@ -229,30 +231,33 @@ module.exports = {
             format: [`PascalCase`, `camelCase`],
             leadingUnderscore: `allowSingleOrDouble`,
           },
-        ]
-      }
+        ],
+      },
     },
 
     {
-      files: ["*.html"],
-      "extends": [
-        "plugin:@angular-eslint/template/recommended",
-      ],
-      "rules": {    "@angular-eslint/template/conditional-complexity": [
-        "error", { "maxComplexity": 4 }],
-      "@angular-eslint/template/cyclomatic-complexity": [
-        "error", { "maxComplexity": 5 }],
-      "@angular-eslint/template/no-duplicate-attributes": "error",
-      "@angular-eslint/template/use-track-by-function": "error"}
+      files: ['*.html'],
+      extends: ['plugin:@angular-eslint/template/recommended'],
+      rules: {
+        '@angular-eslint/template/conditional-complexity': [
+          'error',
+          { maxComplexity: 4 },
+        ],
+        '@angular-eslint/template/cyclomatic-complexity': [
+          'error',
+          { maxComplexity: 5 },
+        ],
+        '@angular-eslint/template/no-duplicate-attributes': 'error',
+        '@angular-eslint/template/use-track-by-function': 'error',
+      },
     },
     {
-      "files": ["*.html"],
-      "excludedFiles": ["*inline-template-*.component.html"],
-      "extends": ["plugin:prettier/recommended"],
-      "rules": {
-        "prettier/prettier": ["error", { "parser": "angular" }]
-      }
-    }
-  ]
-}
-
+      files: ['*.html'],
+      excludedFiles: ['*inline-template-*.component.html'],
+      extends: ['plugin:prettier/recommended'],
+      rules: {
+        'prettier/prettier': ['error', { parser: 'angular' }],
+      },
+    },
+  ],
+};
